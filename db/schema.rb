45c08dfe7dd5240561291f8500be4d9beb52952d
slatersan1 <cdlee212@gmail.com>
2016-03-30 09:24:14 -0400

    update schema include comment table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328142511) do
+ActiveRecord::Schema.define(version: 20160329203016) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text    "content"
+    t.integer "post_id"
+  end
 
   create_table "post_tags", force: :cascade do |t|
     t.integer  "tag_id"
